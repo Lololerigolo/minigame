546c4b34796c59e468c21ad5c91eabca41ad9c04
lololerigolo <laurent.gautier04@gmx.fr>
2020-12-20 17:18:19 +0100

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,2 +1,30 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var lineth = 2;
+var cellsize = 10;
+
+document.write(canvas.width/cellsize);
+
+function drawLine(x,y,dx,dy, color) {
+  context.beginPath();
+  context.moveTo(x,y);
+  context.lineTo(dx, dy);
+  context.fillStyle = color;
+  context.stroke();
+  context.closePath();
+}
+
+function drawGrid() {
+  for (var x = 0; x < canvas.width/cellsize; x++) {
+    for (var y = 0; y < canvas.height/cellsize; x++) {
+      drawLine(x,y,canvas.width,canvas.height,"green");
+    }
+
+  }
+
+}
+drawGrid()
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+}
